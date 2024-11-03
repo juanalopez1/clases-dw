8a8c26c6e58b1d6281327ea22c6ffcd56aa44316
Cristian <CrisRPia>
2024-11-03 16:58:39 -0300

    it works

--- back/src/routes/public/index.ts
+++ back/src/routes/public/index.ts
@@ -1,40 +1,42 @@
 import {
-  FastifyPluginAsyncTypebox,
+    FastifyPluginAsyncTypebox,
+    Type,
 } from "@fastify/type-provider-typebox";
-import { IdUsuarioSchema } from "../../types/usuario.js";
 import { BinarySchema } from "../../types/file.js";
 import fs from "fs";
 import path from "path";
 
 export default (async (fastify) => {
-  fastify.get("/:id_usuario", {
-    schema: {
-      params: IdUsuarioSchema,
-      response: {
-        200: BinarySchema,
-      },
-    },
-    handler: async function (request, reply) {
-      const filePath = path.join(
-        process.cwd(),
-        "src",
-        "public",
-        request.params.id_usuario.toString()
-      );
+    fastify.get("/:filename", {
+        schema: {
+            params: Type.Object({
+                filename: Type.String(),
+            }),
+            response: {
+                200: BinarySchema,
+            },
+        },
+        handler: async function (request, reply) {
+            const filePath = path.join(
+                process.cwd(),
+                "src",
+                "public",
+                request.params.filename
+            );
 
-      try {
-        const file = await fs.promises.readFile(filePath);
-        return reply.code(200).send(file);
-      } catch {
-        const filePath = path.join(
-          process.cwd(),
-          "src",
-          "public",
-          "default.jpeg"
-        );
-        const defaultFile = await fs.promises.readFile(filePath);
-        return reply.code(200).send(defaultFile);
-      }
-    },
-  });
+            try {
+                const file = await fs.promises.readFile(filePath);
+                return reply.code(200).send(file);
+            } catch {
+                const filePath = path.join(
+                    process.cwd(),
+                    "src",
+                    "public",
+                    "default.jpeg"
+                );
+                const defaultFile = await fs.promises.readFile(filePath);
+                return reply.code(200).send(defaultFile);
+            }
+        },
+    });
 }) satisfies FastifyPluginAsyncTypebox;
